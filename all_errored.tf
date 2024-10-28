210b7d850f545f57a9826b3121366965a86ad6a0
Heshercat <alfiiaqa@gmail.com>
2024-10-28 19:14:19 +0200

    valid module

--- all_errored.tf
+++ all_errored.tf
@@ -1,13 +1,36 @@
-resource "null_resource" "name"
+# terraform {
+#  required_version = "1.9.6"
+# }
+
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8200
+      external = 8300
+      protocol = "tcp-ip"
+    }
+  ]
+}
+
+resource "null_resource" "check_ip" {
   triggers = {
-    trigger = var.var
+    current_time = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "curl -s https://ifconfig.me/ip > ip.txt"
   }
 }
 
-variable "var" {
-  default = "String value"
+data "local_file" "read_ip" {
+  depends_on = [null_resource.check_ip]
+  filename   = "./ip.txt"
 }
 
-output "out" {
-  value = var.var
+output "scalr_ip" {
+  value = "Current instance IP is: ${data.local_file.read_ip.content}\nDocker ports information is: ${jsonencode(var.docker_ports)}"
 }
